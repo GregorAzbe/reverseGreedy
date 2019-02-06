8f80bcf9723ffb0ee30edfa2970c695d80a063f0
Gregor <gregor.azbe@gmail.com>
2019-02-06 16:33:50 +0100

    Extracted reverse greedy iteration in dominating set problem

--- src/si/fri/problems/DominatingSet.java
+++ src/si/fri/problems/DominatingSet.java
@@ -30,57 +30,51 @@ public class DominatingSet implements IProblem {
     @Override
     public Solution reverseGreedy(IGraph g) {
         AdjacencyMatrixGraph graph = new AdjacencyMatrixGraph(g);
-
         AbstractCollection<Integer> dominantVertices = new ArrayList<>();
-
         int n = graph.getOriginalSize();
         int[] covCnt = new int[n], score = new int[n];
-
         for (int i = 0; i < n; i++) {
             covCnt[i] = graph.getVertexDeg(i) + 1;
             score[i] = graph.getVertexDeg(i) + 1;
         }
-
-        for (int i = 0; i < n; i++) {
-            int x = 0;
-            int minScore = Integer.MAX_VALUE;
-
-            for (int j = 0; j < score.length; j++) {
-                if (score[j] < minScore){
-                    minScore = score[j];
-                    x = j;
-                }
-            }
-
-            List<Integer> neighbours = graph.getNeighbours(x);
-            neighbours.add(x);
-
-            boolean isDominant = false;
-            for (int neighbour : neighbours){
-                if(covCnt[neighbour] == 1){
-                    dominantVertices.add(x);
-
-                    covCnt[x] = 0;
-                    for (int neighbour1 : neighbours){
-                        covCnt[neighbour1] = 0;
-                    }
-                    isDominant = true;
-                    break;
-                }
-            }
-
-            if(!isDominant){
-                for (int neighbour : neighbours){
-                    if(covCnt[neighbour] > 0){
-                        covCnt[neighbour]--;
-                        score[neighbour]++;
-                    }
-                }
-            }
-            score[x] = Integer.MAX_VALUE/2;
-        }
+        for (int i = 0; i < n; i++)
+            makeReverseGreedyIteration(graph, covCnt, score, dominantVertices);
 
         return new VertexSet(dominantVertices);
+    }
+
+    private void makeReverseGreedyIteration(AdjacencyMatrixGraph graph, int[] covCnt, int[] score, AbstractCollection<Integer> dominantVertices){
+        int x = 0;
+        int minScore = Integer.MAX_VALUE;
+        for (int j = 0; j < score.length; j++) {
+            if (score[j] < minScore){
+                minScore = score[j];
+                x = j;
+            }
+        }
+        List<Integer> neighbours = graph.getNeighbours(x);
+        neighbours.add(x);
+        boolean isDominant = false;
+        for (int neighbour : neighbours){
+            if(covCnt[neighbour] == 1){
+                dominantVertices.add(x);
+                covCnt[x] = 0;
+                for (int neighbour1 : neighbours){
+                    covCnt[neighbour1] = 0;
+                }
+                isDominant = true;
+                break;
+            }
+        }
+        if(!isDominant){
+            for (int neighbour : neighbours){
+                if(covCnt[neighbour] > 0){
+                    covCnt[neighbour]--;
+                    score[neighbour]++;
+                }
+            }
+        }
+        score[x] = Integer.MAX_VALUE/2;
     }
 
     @Override
