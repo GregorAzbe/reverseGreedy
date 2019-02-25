24c0993d048be15736222787581aa7e9b7943238
Gregor <gregor.azbe@gmail.com>
2019-02-25 11:05:03 +0100

    Update chart script

--- Charts/charts.py
+++ Charts/charts.py
@@ -9,8 +9,10 @@ import sys
 
 GREEDY_COLOR = dict(color='rgb(255,127,14)')
 REVERSE_GREEDY_COLOR = dict(color='rgb(31,119,180)')
-FILE_NAME = "../Dominating set - final.csv"
-PROBLEM_NAME = "Domnantna množica"
+FILE_NAME = "../Barvanje grafov - final.csv"
+PROBLEM_NAME = "Barvanje grafov"
+X_AXIS_LABEL = "Število povezav v grafu"
+FINAL = False
 
 
 def get_scatters(results, avgs, name, column_name, color):
@@ -32,15 +34,17 @@ def get_scatters(results, avgs, name, column_name, color):
     return [measurements, avg]
 
 
-def make_chart(name, y_axis_label, greedy_column_index, reverse_gredy_column_index, unit=""):
+def make_chart(name, results, x_axis_label, y_axis_label, greedy_column_index, reverse_gredy_column_index, use_median, unit=""):
     results_df = ps.DataFrame(results[:, (0, greedy_column_index, reverse_gredy_column_index)],
                               columns=['n_problem', 'greedy', 'reverse_greedy'])
-    avgs = results_df.groupby(['n_problem']).apply(
-        lambda x: (np.average(x['greedy']), np.average(x['reverse_greedy']))).apply(ps.Series)
+
+    avgs_or_medians = results_df.groupby(['n_problem']).apply(
+        lambda x: (np.median(x['greedy']) if use_median else np.average(x['greedy']), np.median(x['reverse_greedy']) if use_median else np.average(x['reverse_greedy']))
+    ).apply(ps.Series)
 
     greedy_scatters = get_scatters(
         results_df,
-        avgs[0],
+        avgs_or_medians[0],
         "Požrešni algoritem",
         'greedy',
         GREEDY_COLOR
@@ -48,8 +52,8 @@ def make_chart(name, y_axis_label, greedy_column_index, reverse_gredy_column_ind
 
     reverse_greedy_scatters = get_scatters(
         results_df,
-        avgs[1],
-        "Obratni pozresni algoritem",
+        avgs_or_medians[1],
+        "Izločevalni algoritem",
         'reverse_greedy',
         REVERSE_GREEDY_COLOR
     )
@@ -58,7 +62,7 @@ def make_chart(name, y_axis_label, greedy_column_index, reverse_gredy_column_ind
     layout = go.Layout(
         title=name,
         xaxis=dict(
-            title='Število vozlišč v grafu',
+            title=x_axis_label,
             titlefont=dict(
                 family='Courier New, monospace',
                 size=18,
@@ -78,21 +82,31 @@ def make_chart(name, y_axis_label, greedy_column_index, reverse_gredy_column_ind
     figure = go.Figure(data=data, layout=layout)
     py.plot(figure, filename=name)
 
+def make_charts(file_name, problem_name, x_axis_label):
+    results = np.loadtxt(file_name, delimiter=";")
+    results[:, 2] = results[:, 2] / 1000000
+    results[:, 4] = results[:, 4] / 1000000
+
+    plotly.tools.set_credentials_file(username='gregor.azbe', api_key='ynSyJDs4GCTyGD4oBkAO')
+    make_chart(problem_name + " - primerjava uspešnosti algoritmov", results, x_axis_label, "Velikost rešitve", 1, 3, False)
+    make_chart(problem_name + "  - primerjava učinkovitosti algoritmov", results, x_axis_label, "Čas računanja", 2, 4, True, "ms")
 
 if len(sys.argv) > 1:
     try:
-        _, file_name, problem_name = sys.argv
+        _, file_name, problem_name, x_axis_label = sys.argv
     except ValueError as e:
         print("Unpack: ", sys.argv, file=sys.stderr)
         raise
+    make_charts(file_name, problem_name, x_axis_label)
+
 else:
-    file_name = FILE_NAME
-    problem_name = PROBLEM_NAME
+    if FINAL:
+        make_charts("../Dominating set - final.csv", "Dominantna množica", "Število vozlišč v grafu")
+        make_charts("../Barvanje grafov - final.csv", "Barvanje grafov", "Število povezav v grafu")
+        make_charts("../Vertex cover - final.csv", "Vozliščno pokritje", "Število vozlišč v grafu")
 
-results = np.loadtxt(file_name, delimiter=";")
-results[:, 2] = results[:, 2] / 1000000
-results[:, 4] = results[:, 4] / 1000000
-
-plotly.tools.set_credentials_file(username='gregor.azbe', api_key='ynSyJDs4GCTyGD4oBkAO')
-make_chart(problem_name + " - primerjava uspešnosti algoritmov", "Velikost rešitve", 1, 3)
-make_chart(problem_name + "  - primerjava učinkovitosti algoritmov", "Čas računanja", 2, 4, "ms")
+    else:
+        file_name = FILE_NAME
+        problem_name = PROBLEM_NAME
+        x_axis_label = X_AXIS_LABEL
+        make_charts(file_name, problem_name, x_axis_label)
